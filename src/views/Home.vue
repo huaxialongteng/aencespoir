3c3da7f9a14ca682a4ea236947fa6593e01e89b7
jinming <aencespoir@gmail.com>
2021-10-21 21:54:15 +0800

    优化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class='home'></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'home',
+    data() {
+        return {
+            //data
+            //bool
+            //ul
+        };
+    },
+    props: {},
+    components: {},
+    methods: {},
+    watch: {},
+    created() { },
+    mounted() { },
+};
 </script>
+
+<style lang='scss' scoped>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    chainWebpack: (config) => {
+        config.plugin("html").tap((args) => {
+            args[0].title = "Aencespoir";
+            return args;
+        });
+    },
+    publicPath: '/',
+    devServer: {
+        port: 8000,
+        proxy: {
+            '/public': {
+                target: 'http://192.168.1.7:4000',
+                changeOrigin: true,
+                ws: true
+            }
+        }
+    }
+}
